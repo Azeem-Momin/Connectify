41cd9a244040b0641d8d9298285376236c0d6bb6
azeem <azeemmomin369@gmail.com>
2025-02-11 18:23:38 +0530

    resolved

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,5 +1,5 @@
 let IS_PROD = true;
-const server = IS_PROD ? "https://connectify-jswj.onrender.com" : "http://localhost:8000"
+const server = IS_PROD ? "https://connectify-2hh5.onrender.com" : "http://localhost:8000"
 
 // const server = IS_PROD ? "http://localhost:8000":  "https://connectify-jswj.onrender.com"
 
